82c8c494a70d3dc80dd7f55fee1c6a1e3d9d532d
Mustafa Zuberi <mustafazuberi986@gmail.com>
2023-05-01 07:21:31 +0500

    10 tutorials completed

---
+++ hunting-coder/pages/blogpost/[slug].js
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router"
+
+const slug = () => {
+    const router = useRouter()
+    const { slug } = router.query
+
+    return (
+        <div>
+            <h1>{slug}</h1>
+        </div>
+    )
+}
+
+export default slug
